414e4490d7f2d4007292f6e5f2cf583940064451
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-03-04 00:25:22 +0500

    Set up the project

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import {View, FlatList, Text, Image, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <View style={styles.root}></View>;
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+});
+
+export default App;
