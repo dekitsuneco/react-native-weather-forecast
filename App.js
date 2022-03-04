6b64683d0a4b6ba1265dab47d7f641446ef80f9e
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-03-05 02:58:15 +0500

    Add logic of fetching data from API

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
 import SettingsBlock from './components/SettingsBlock/SettingsBlock';
 import WidgetBlock from './components/WidgetBlock/WidgetBlock';
 import InfoBlock from './components/InfoBlock/InfoBlock';
 
 const App = () => {
-  const weatherData = {
+  /*const weatherData = {
     temperature: 20,
     description: 'Mostly sunny',
     status: 'sun',
@@ -13,7 +13,66 @@ const App = () => {
     humidity: 60,
     pressure: 752,
     rainProb: 10,
+  };*/
+  const [weatherData, setWeatherData] = useState({});
+
+  const normalizeDataFromAPI = data => {
+    const normalizeTemp = temp => Math.round(temp - 273.15);
+    const normalizeWind = wind => Math.round(wind);
+    const normalizeHumidity = humidity => Math.round(humidity);
+    const normalizePressure = pressure => Math.round(pressure * 0.75006);
+    const normalizeRainProb = rainProb => Math.round(rainProb);
+    const normalizeDescription = description =>
+      description[0].toUpperCase() + description.substring(1);
+    const normalizeStatus = status => {
+      switch (status) {
+        case 'Thunderstorm':
+          return 'storm';
+        case 'Clouds':
+          return 'cloud';
+        case 'Clear':
+          return 'sun';
+        case 'Drizzle':
+        case 'Rain':
+        case 'Snow': //TODO
+          return 'rain';
+        default:
+          //TODO
+          return 'cloud';
+      }
+    };
+
+    return {
+      temperature: normalizeTemp(data.main.temp),
+      description: normalizeDescription(data.weather[0].description),
+      status: normalizeStatus(data.weather[0].main),
+      wind: normalizeWind(data.wind.speed),
+      humidity: normalizeHumidity(data.main.humidity),
+      pressure: normalizePressure(data.main.pressure),
+      rainProb: normalizeRainProb(10), //TODO
+    };
   };
+  const fetchWeatherDataFromAPI = () => {
+    const URL =
+      'https://api.openweathermap.org/data/2.5/weather?q=London&appid=bce68de2a52a0351de2783eff7e40797';
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        const normalizedWeatherData = normalizeDataFromAPI(data);
+        setWeatherData(normalizedWeatherData);
+
+        console.log(normalizedWeatherData);
+      });
+  };
+
+  useEffect(() => {
+    fetchWeatherDataFromAPI();
+
+    /*return () => {
+      second;
+    };
+  }, [third]);*/
+  });
 
   const {temperature, description, status, ...weatherReport} = weatherData;
 
