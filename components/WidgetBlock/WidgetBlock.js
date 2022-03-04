7461ec20a0f7644af66d06887505e7c668a1b419
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-03-04 19:03:47 +0500

    Refacture main block

---
+++ components/WidgetBlock/WidgetBlock.js
@@ -0,0 +1,54 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Main = () => {
+  return (
+    <View style={styles.widgetBlock}>
+      <View style={styles.topRow}>
+        <View style={styles.weatherIcon}>
+          <Text style={styles.icon}>⛅</Text>
+        </View>
+        <View style={styles.weatherTemp}>
+          <Text style={styles.temp}>19º</Text>
+        </View>
+      </View>
+      <View style={styles.bottomRow}>
+        <View style={styles.weatherDescription}>
+          <Text style={styles.description}>Mostly sunny</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Main;
+
+const styles = StyleSheet.create({
+  widgetBlock: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topRow: {
+    flexDirection: 'row',
+  },
+  weatherIcon: {},
+  icon: {
+    color: 'white',
+    fontSize: 120,
+  },
+  weatherTemp: {},
+  temp: {
+    color: 'white',
+    fontSize: 120,
+    fontFamily: 'Lato',
+  },
+  bottomRow: {
+    textAlign: 'center',
+  },
+  weatherDescription: {},
+  description: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
