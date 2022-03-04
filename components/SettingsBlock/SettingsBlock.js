7cee67d75a8e57f7bb515886346ba2941fc3a0a1
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-03-04 18:45:06 +0500

    Fix path bug

--- components/SettingsBlock/SettingsBlock.js
+++ components/SettingsBlock/SettingsBlock.js
@@ -1,8 +1,5 @@
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
-import CityTitle from '../Header/CityTitle/CityTitle';
-import TemperatureSwitch from '../Header/TemperatureSwitch/TemperatureSwitch';
-import LocationSwitch from '../Header/LocationSwitch/LocationSwitch';
 
 const SettingsBlock = () => {
   return (
